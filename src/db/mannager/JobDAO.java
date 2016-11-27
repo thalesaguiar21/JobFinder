203b6ae057f651fcff8a2eb30a42a72a7cb2d994
thalesaguiar21 <thalesaguiar21@gmail.com>
2016-11-27 06:58:26 -0300

    Implementação de JobDAO concluída.

--- src/db/mannager/JobDAO.java
+++ src/db/mannager/JobDAO.java
@@ -1,7 +1,7 @@
 package db.mannager;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import framework.DadosGlobais;
 
@@ -13,23 +13,40 @@ public class JobDAO {
 	private String requirements;
 	private double rem;
 	private String companyName;*/
-	public boolean insertJob ( String title, String postDate, String role
-						  	 , String requeriments, String rem, String companyName){
-		DadosGlobais.getDados().getMyDb().update("INSERT INTO Job (jobTitle, jobRole, jobRequirements" +
-						  	                     ", jobPostDate");
+	public boolean insertJob (Job job, int companyId){
+		String query = "INSERT INTO Job(company_idCompany, jobTitle, jobRole, jobRequierements, jobPostDate, jobRem) "
+				+ "VALUES (" + companyId + ", '" + job.getTitle() + ", '" + job.getRole() + ", '" + job.getRequirements()
+				+ ", '" + job.getPostDate() + ", '" + job.getRem() + "');";
+		DadosGlobais.getDados().getMyDb().update(query);
 		return true;
 	}
 	
 	public boolean removeJob (String title) {
+		String query = "DELETE FROM Job WHERE jobTitle = '" + title + "';";
+		DadosGlobais.getDados().getMyDb().update(query);
 		return true;
 	}
 	
-	public List<Job> getJob (String title){
-		List<Job> foundJobs = new ArrayList<Job>();
-		return foundJobs;
+	public Job getJob (String title){
+		String query = "SELECT * FROM Job WHERE jobTitle = '" + title + "' LIMIT 1;";
+		
+		List<Map<String, Object>> result = DadosGlobais.getDados().getMyDb().select(query);
+		String tit   = (String) result.get(0).get("jobtitle");
+		String role  = (String) result.get(0).get("jobrole");
+		String req   = (String) result.get(0).get("jobrequeirements");
+		String date  = (String) result.get(0).get("jobpostdate");
+		String rem   = (String) result.get(0).get("jobrem");
+		
+		Job job = new Job(tit, date, role, req, Double.parseDouble(rem));
+		return job;
 	}
 	
-	public boolean updateJob (String title) {
+	public boolean updateJob (Job oldJob, Job newJob) {
+		String query = "UPDATE Job SET jobTitle = '" + newJob.getTitle() + "', jobRole = '"
+				+ newJob.getRole() + "', jobRequirements = '" + newJob.getRequirements()
+				+ "', jobPostDate = '" + newJob.getPostDate() + "', jobRem = " + newJob.getRem()
+				+ " WHERE jobTitle = '" + oldJob.getTitle() + "');";
+		DadosGlobais.getDados().getMyDb().update(query);
 		return true;
 	}
 
